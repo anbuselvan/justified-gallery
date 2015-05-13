9a26415897a2d1e50f1abad973426fc6aa3f4aa7
Anbuselvan Periannan <anbuselvan.p@gmail.com>
2015-05-12 21:42:28 -0700

    added swipebox

--- package.js
+++ package.js
@@ -1,23 +1,23 @@
 Package.describe({
   name: 'anbuselvan:justified-gallery',
   summary: 'Another awesome gallery for Meteor.',
-  version: '0.0.1',
+  version: '0.0.2',
   git: 'https://github.com/anbuselvan/justified-gallery.git'
 });
 
 Package.onUse(function (api) {
-  api.use(['jquery','twbs:bootstrap'], 'client');
+  api.use(['jquery'], 'client');
 
   api.addFiles([
-    "vendor/Justified-Gallery/libs/swipebox/img/icons.png",
-    "vendor/Justified-Gallery/libs/swipebox/img/icons.svg",
-    "vendor/Justified-Gallery/libs/swipebox/img/loader.gif"
+    "vendor/swipebox/src/img/icons.png",
+    "vendor/swipebox/src/img/icons.svg",
+    "vendor/swipebox/src/img/loader.gif"
   ], "client", { isAsset: true });
 
   api.addFiles([
     'vendor/Justified-Gallery/dist/css/justifiedGallery.css',
     'vendor/Justified-Gallery/dist/js/jquery.justifiedGallery.js',
-    'vendor/Justified-Gallery/libs/swipebox/swipebox.css',
-    'vendor/Justified-Gallery/libs/swipebox/jquery.swipebox.min.js'
+    'vendor/swipebox/src/css/swipebox.min.css',
+    'vendor/swipebox/src/js/jquery.swipebox.min.js'
   ], 'client');
 });
